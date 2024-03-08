9baf3a0dc71137a9150185cd0ec232aa7f6c2764
Sergii Minko <minkosergii@gmail.com>
2024-03-08 11:29:31 +0100

    test-2-end

--- py/test-2.py
+++ py/test-2.py
@@ -12,15 +12,30 @@ def get_numbers_ticket(min, max, quantity):
 
 
 string = "1, 49, 6"
-numbers = [int(x) for x in string.split(",")]
-min_value = numbers[0]
-max_value = numbers[1]
-quantity = numbers[2]
+string = input("Введіть діапазон чисел min, max, quantity: ")
+if (string != "") & (string.count(",") == 2):
+    try:
+        numbers = [int(x) for x in string.split(",")]
+        min_value = numbers[0]
+        max_value = numbers[1]
+        quantity = numbers[2]
+    except:
+        print("Неправильний формат даних")
+else:
+    print("Неправильний формат даних")
 
+if min_value > max_value:
+    min_value, max_value = max_value, min_value
+
+if max_value - min_value + 1 < quantity:
+    print("Неправильний формат даних quantity має бути меншим за max - min + 1")
+    quantity = (max_value - min_value) // 2
+    print("Нова кількість чисел: ", quantity)
+
+print("отримані данні для лотерейнього білету:")
 print(f"Мінімальне число: {min_value}")
 print(f"Максимальне число: {max_value}")
 print(f"Кількість чисел: {quantity}")
-
 
 lottery_numbers = get_numbers_ticket(min_value, max_value, quantity)
 print("Ваші лотерейні числа:", lottery_numbers)
